714174808b312a6853e7b89e6e732c15baf6a7ff
pico4 <pico4@web.de>
2022-07-22 15:36:01 +0200

    Try removing int64_t problem

--- src/timer.c
+++ src/timer.c
@@ -4,21 +4,25 @@
 #include "hardware/watchdog.h"
 #include "hardware/clocks.h"
 
+#include <stdint.h>
+
 #include "timer.h"
 #include "blinker.h"
 
 int counter = 0;
 
-unsigned long timer_callback(alarm_id_t id, void *user_data)
+uint64_t timer_callback(alarm_id_t id, void *user_data)
 {
     counter++;
     return 0;
 }
 
+alarm_callback_t p = timer_callback;
+
 void timer_init()
 {
     // Timer example code - This example fires off the callback after 2000ms
-    add_alarm_in_ms(2000, timer_callback, NULL, false);
+    add_alarm_in_ms(2000, p, NULL, false);
 
     puts("\nLooper init done");
 
